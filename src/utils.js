2b513fb3f3d49e6b737bf6cfab88f8c0ff52487b
Jordan Rand <jorrand@gmail.com>
2018-10-04 10:25:39 -0600

    init and simple tests

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const sum = (x, y) => x + y
+
+export const capitialize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
---
+++ src/utils.test.js
@@ -0,0 +1,16 @@
+import {
+  sum, 
+  capitialize,
+} from './utils';
+
+it ('adds two numbers', () => {
+  const expected = 5;
+  const actual = sum(2,3)
+  expect(actual).toEqual(expected)
+})
+
+it ('capitalizes a string', () => {
+  const expected = "Hello world";
+  const actual = capitialize("hello world")
+  expect(actual).toEqual(expected)
+})
